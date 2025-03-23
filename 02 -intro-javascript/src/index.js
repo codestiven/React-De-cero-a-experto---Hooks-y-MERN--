c2c800b188bec98a3b6bad70244617e14880cfac
Stiven de la rosa <108486081+codestiven@users.noreply.github.com>
2025-03-23 14:25:06 -0400

    18. Arreglos

--- 02 -intro-javascript/src/index.js
+++ 02 -intro-javascript/src/index.js
@@ -1,12 +1,17 @@
-const nombre = "Stiven";
-const apellido = "de la rosa ";
+// Arreglos en JS
+// const arreglo = new Array(100);
+const arreglo = [1, 2, 3, 4];
+// arreglo.push(1)
+// arreglo.push(2)
+// arreglo.push(3)
+// arreglo.push(4)
 
-const nombrecompleto = `${nombre} ${apellido}`;
+let arreglo2 = [...arreglo, 5];
 
+const arreglo3 = arreglo2.map(function (numero) {
+    return numero * 2;
+});
 
-
-function saludar(name) {
-    return `Hola ${name}`;
-}
-
-console.log(saludar(nombrecompleto));
+console.log(arreglo);
+console.log(arreglo2);
+console.log(arreglo3);
