bf5c436fa6a7e8f57ad08ca33a90c02422a4e4b0
Stiven de la rosa <108486081+codestiven@users.noreply.github.com>
2025-03-23 13:57:40 -0400

    16. Template String

--- 02 -intro-javascript/src/index.js
+++ 02 -intro-javascript/src/index.js
@@ -1,1 +1,12 @@
-console.log("Hola, soy Stiven");
+const nombre = "Stiven";
+const apellido = "de la rosa ";
+
+const nombrecompleto = `${nombre} ${apellido}`;
+
+
+
+function saludar(name) {
+    return `Hola ${name}`;
+}
+
+console.log(saludar(nombrecompleto));
