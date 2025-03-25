030ea7d3608248276cecfa6fb0640f0174985e61
Stiven de la rosa <108486081+codestiven@users.noreply.github.com>
2025-03-25 18:33:24 -0400

    37. Nuestro primer Componente

--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-function App() {
-  return <h1>Hello, world!</h1>;
-}
+import App from './HelloWordApp';
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />);
