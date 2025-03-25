c219f086d7bc6984118e17b252d4503fb2e349d1
Stiven de la rosa <108486081+codestiven@users.noreply.github.com>
2025-03-25 18:45:38 -0400

    38. Tarea - Crear un nuevo componente

---
+++ 03-counter-app-vite/src/FistApp.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FistApp = () => {
+  return (
+    <div>
+      <h1>Stiven</h1>
+    </div>
+  )
+}
+
+export default FistApp
--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -2,5 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './HelloWordApp';
+import FistApp from './FistApp';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(<FistApp />);
