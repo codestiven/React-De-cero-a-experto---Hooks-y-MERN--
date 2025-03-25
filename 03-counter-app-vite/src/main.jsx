bd2ddbfa9ec9ea0a97e30ecec8847495f416989e
Stiven de la rosa <108486081+codestiven@users.noreply.github.com>
2025-03-25 17:59:03 -0400

    36. Hola Mundo en React

--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -1,10 +1,8 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+function App() {
+  return <h1>Hello, world!</h1>;
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
