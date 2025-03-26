4873daef2036649a3a81774ce499dd84004b4586
Stiven de la rosa <108486081+codestiven@users.noreply.github.com>
2025-03-26 16:45:22 -0400

    40. Impresión de variables en el HTML

--- 03-counter-app-vite/src/FistApp.jsx
+++ 03-counter-app-vite/src/FistApp.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
 
+
+const getsaludo = () => {
+  return `Hola Mundo ${new Date().getFullYear()}`
+}
+
 const FistApp = () => {
+
   return (
     <>
+      <h1>{getsaludo()}</h1>
       <h1>Stiven</h1>
     </>
   )
