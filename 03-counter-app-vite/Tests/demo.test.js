4a5ddfb0ed1a0b93b278ca1478e4f69a664d4c73
Stiven de la rosa <108486081+codestiven@users.noreply.github.com>
2025-03-29 16:49:15 -0400

    55. Jest - Expect - toBe

--- 03-counter-app-vite/Tests/demo.test.js
+++ 03-counter-app-vite/Tests/demo.test.js
@@ -1,8 +1,21 @@
-test('Esta prueba no deberia de fallar ', () => {
-  
-    if (1 === 0) {
-        throw new Error('No deberia dividir por cero')
+describe('pruevas en <democomponent />', () => {
 
-        
-    }
+    test('Esta prueba no deberia de fallar ', () => {
+
+        // 1. Inicializacion
+        const membro = "Fernando";
+
+        //2. Estimulo
+        const mensaje2 = membro.trim();
+
+        //3. Observar el resultado
+        expect(mensaje2).toBe("Fernando");
+    });
 })
+
+
+
+
+
+
+
