206e6fbcd4a8463e57b109054877aaed83b90cf1
Stiven de la rosa <108486081+codestiven@users.noreply.github.com>
2025-04-01 21:15:17 -0400

    66. Probar FirstApp

--- 03-counter-app-vite/Tests/FistApp.test.jsx
+++ 03-counter-app-vite/Tests/FistApp.test.jsx
@@ -5,10 +5,27 @@ import React from 'react';
 describe('Pruevas en <FistApp/>', () => {
 
     test('debe de hacer match con el snapshot', () => {
+        const titulo = 'Hola, soy Goku'
 
-        render(<FistApp title="Hola, soy Goku" subtitle="Soy un subtitulo" />)
+        const {container} = render(<FistApp title={titulo} subtitle="Soy un subtitulo" />)
+
+        expect(container).toMatchSnapshot()
       
     })
+
+    test ('debe de mostrar el titulo en un h1', () => {
+        const titulo = 'Hola, soy Goku'
+
+        const {container,getByText} = render(<FistApp title={titulo} subtitle="Soy un subtitulo" />)
+
+        expect(getByText(titulo)).toBeTruthy()
+
+        const h1 = container.querySelector('h1');
+        expect(h1.innerHTML).toContain(titulo)
+    })
+
+
+
     
   
 })
