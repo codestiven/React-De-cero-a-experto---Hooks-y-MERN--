097d6750a8a0509e8c0ade6d9ae25e560b4fc245
Stiven de la rosa <108486081+codestiven@users.noreply.github.com>
2025-04-08 16:19:39 -0400

    77. Creando una lista de categorias

--- 04-GifExpertApp/gifgrabber/src/GifExpertApp.jsx
+++ 04-GifExpertApp/gifgrabber/src/GifExpertApp.jsx
@@ -1,12 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function GifExpertApp() {
+  
+  const [categories, setCategories] = useState(['One Punch', 'Samurai X', 'Dragon Ball']);
+
+
+
+
   return (
     <>
 
     {/* titulo */}
 
     <h1>hola mundo</h1>
+
+    <ol>
+      {
+        categories.map((category) => {
+          return <li key={category}>{category}</li>
+        })
+      }
+    </ol>
 
 
     {/* input */}
