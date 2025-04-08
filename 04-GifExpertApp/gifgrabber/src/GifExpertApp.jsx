3d1ab96024b13eb5f728299e91a9b5323dd81e70
Stiven de la rosa <108486081+codestiven@users.noreply.github.com>
2025-04-08 16:11:10 -0400

    76. GifExpertApp - Component

--- 04-GifExpertApp/gifgrabber/src/GifExpertApp.jsx
+++ 04-GifExpertApp/gifgrabber/src/GifExpertApp.jsx
@@ -4,8 +4,16 @@ function GifExpertApp() {
   return (
     <>
 
+    {/* titulo */}
+
     <h1>hola mundo</h1>
-      
+
+
+    {/* input */}
+
+    {/* listado de gifs */}
+         {/* gif item */}
+
     </>
   )
 }
