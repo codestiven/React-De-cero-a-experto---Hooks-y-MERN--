d8d8d3d0d6475ad49c34e0c2134f99c3a8abad7c
Stiven de la rosa <108486081+codestiven@users.noreply.github.com>
2025-04-24 15:19:41 -0400

    Hacer un mock completo de un Custom Hook

--- 04-GifExpertApp/gifgrabber/Tests/Components/GifGrid.test.jsx
+++ 04-GifExpertApp/gifgrabber/Tests/Components/GifGrid.test.jsx
@@ -1,17 +1,52 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { GifGrid } from "../../src/Components/GifGrid";
+import { useFetchGifs } from "../../src/Hooks/useFetchGifs";
 
-describe('Pruebas en <GifGrid/>', () => {
+// 👇 Asegúrate de mockear antes de cualquier test
+jest.mock("../../src/Hooks/useFetchGifs");
+
+describe('Pruebas en <GifGrid />', () => {
 
     const category = 'One Punch';
 
-    test('Debe de mostrar el loading inicialmente ', () => {
-      
+    test('Debe de mostrar el loading inicialmente', () => {
 
-        render(<GifGrid category={category} />)
-        expect(screen.getByText('Loading...')).toBeTruthy(); // Verifica que el texto "Loading..." esté presente en el DOM
-        expect(screen.getByText(category)).toBeTruthy(); // Verifica que el nombre de la categoría esté presente en el DOM
-    })
-    
-  
-})
+        // 👈 Aquí agregamos un valor de retorno inicial
+        useFetchGifs.mockReturnValue({
+            images: [],
+            isLoading: true
+        });
+
+        render(<GifGrid category={category} />);
+
+        expect(screen.getByText('Loading...')).toBeTruthy();
+        expect(screen.getByText(category)).toBeTruthy();
+    });
+
+    test('Debe de mostrar items cuando se cargan las imagenes useFetchGifs', () => {
+
+        const gifs = [
+            {
+                id: 'ABC',
+                title: 'Saitama',
+                url: 'https://localhost/saitama.jpg'
+            },
+            {
+                id: '123',
+                title: 'Goku',
+                url: 'https://localhost/goku.jpg'
+            }
+        ];
+
+        useFetchGifs.mockReturnValue({
+            images: gifs,
+            isLoading: false
+        });
+
+        render(<GifGrid category={category} />);
+
+        // 👇 Verifica que hay 2 imágenes renderizadas
+        expect(screen.getAllByRole('img').length).toBe(2);
+    });
+
+});
